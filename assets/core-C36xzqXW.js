a076ff14c5c919b3bef044ee4af4566ab80a283d
TheGrieferKnight <grieferknight@yahoo.com>
2025-09-08 18:47:51 +0200

    fix: now uses correct icons for tierlist

---
+++ assets/core-C36xzqXW.js
@@ -0,0 +1,1 @@
+import"./index-BiutW40y.js";async function e(n,o={},i){return window.__TAURI_INTERNALS__.invoke(n,o,i)}export{e as invoke};
--- assets/core-DgVhM3kv.js
+++
@@ -1,1 +0,0 @@
-import"./index-4th-tcry.js";async function e(n,o={},i){return window.__TAURI_INTERNALS__.invoke(n,o,i)}export{e as invoke};
